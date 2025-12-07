53b127ab4e82e1f6bed4a44bc2684cabed5b059d
unknown <samadriaz9@gmail.com>
2025-12-07 23:19:39 +0500

    5001

--- app.py
+++ app.py
@@ -930,7 +930,7 @@ if __name__ == '__main__':
     print("="*50 + "\n")
     
     try:
-        app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+        app.run(host='0.0.0.0', port=5001, debug=False, threaded=True)
     finally:
         GPIO.cleanup()
         print("GPIO cleaned up")
